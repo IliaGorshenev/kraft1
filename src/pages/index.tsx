218f3ec71198ec2741f929ae8344dc64ae1f88f7
Ilia Gorshenev <94396603+IliaGorshenev@users.noreply.github.com>
2025-05-22 19:42:24 +0300

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function IndexPage() {
           </div>
         </div>
         <FeatureSection
-          id="about"
+
           leftSlot={
             <div>
               <h2 className="text-3xl font-bold mb-4">О компании</h2>
